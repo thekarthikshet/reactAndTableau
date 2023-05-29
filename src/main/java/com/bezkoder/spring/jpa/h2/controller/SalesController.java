46bd07e5203f8e5828890542aff3fbbee35fa821
thekarthikshet <shetkarthik8888@gmail.com>
2023-05-29 16:05:40 +0530

    added comment2 in controller

--- src/main/java/com/bezkoder/spring/jpa/h2/controller/SalesController.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/controller/SalesController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//new comment 2
 import com.bezkoder.spring.jpa.h2.dto.ProductQuantityDto;
 import com.bezkoder.spring.jpa.h2.dto.TotalSalesDTO;
 import com.bezkoder.spring.jpa.h2.model.Sales;
